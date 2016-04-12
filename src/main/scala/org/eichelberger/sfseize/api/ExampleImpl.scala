13b342c468d59e834f0630e9ca4ce46ace7c5e18
Chris Eichelberger <cne1x@ccri.com>
2016-04-12 07:23:01 -0400

    Started to add logging, but it's not done yet.

--- src/main/scala/org/eichelberger/sfseize/api/ExampleImpl.scala
+++ src/main/scala/org/eichelberger/sfseize/api/ExampleImpl.scala
@@ -4,7 +4,27 @@ package org.eichelberger.sfseize.api
   *  discretizers/cells
   ******************************************************/
 
+// okay, so these aren't just operations on "continuous" spaces,
+// but also on linear/uniform distributions of continuous values...
+
 case class ContinuousFieldRange(minimum: Double, maximum: Double)
   extends FieldRange[Double]
 
 case class ContinuousSpace(ranges: Seq[ContinuousFieldRange]) extends Space[Double]
+
+case class ContinuousDiscretizer(fieldName: String, range: ContinuousFieldRange, cardinality: Long) extends Discretizer[Double] {
+  val rangeSize = range.maximum - range.minimum
+  val binSize = rangeSize / cardinality.toDouble
+
+  def conditioned(data: Double): Double =
+    Math.min(Math.max(data, range.minimum), range.maximum)
+
+  // should return a bin index on [0..c-1]
+  def discretize(data: Double): Long = {
+    Math.min(cardinality - 1, Math.floor((conditioned(data) - range.minimum) / binSize).toLong)
+  }
+
+  def undiscretize(bin: Long): Space[Double] = {
+    ???
+  }
+}
--- src/test/scala/org/eichelberger/sfseize/api/ExampleImplTest.scala
+++ src/test/scala/org/eichelberger/sfseize/api/ExampleImplTest.scala
@@ -5,12 +5,23 @@ import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 
 @RunWith(classOf[JUnitRunner])
-class ExampleImplTest extends Specification {
+class ExampleImplTest extends Specification with LazyLogging {
   "FieldRange" should {
     "be creatable" in {
       val fieldRange = ContinuousFieldRange(0.0, 1.0)
       println(s"Field range:  $fieldRange")
       fieldRange must not(beNull)
+    }
+  }
+
+  "longitude ContinuousDiscretizer" should {
+    "correctly handle Max value" in {
+      for (c <- 1 to 36) {
+        val discretizer = ContinuousDiscretizer("example", ContinuousFieldRange(-180.0, 180.0), 36)
+        val bin = discretizer.discretize(180.0)
+        println(s"")
+        bin must equalTo(c - 1)
+      }
     }
   }
 }
